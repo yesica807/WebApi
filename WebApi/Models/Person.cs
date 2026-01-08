f84de317edfbea31e0b03bcc64d4d01a4bbec7a3
yesica goez <yesica@example.com>
2026-01-08 13:36:36 -0500

    Add project files.

---
+++ WebApi/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public required string Name { get; set; }
+        public required int age { get; set; }
+
+    }
+}
