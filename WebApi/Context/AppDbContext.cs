a3e7eea1b8cadfc3947f17ed827bfb7d9b1608f7
yesica goez <yesica@example.com>
2026-01-09 15:23:31 -0500

    se crea cambio

---
+++ WebApi/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Context
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// crear variable para la cadena de conexion
+var connectionString = builder.Configuration.GetConnectionString("Connection");
+// registrar servicio para la conexion
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer(connectionString)
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
